50bc0f0ff8c1150883b47a971d6c429b1ea59445
Gabriel Sesta <gabrielsesta3@gmail.com>
2023-05-04 23:04:32 +0200

    aggiunto form ed emailjs - aggiunta card con form per ricezione email ed aggiunta file email.js

---
+++ assets/js/email.js
@@ -0,0 +1,29 @@
+
+emailjs.init("FT9ZYNHp3peaBCHrE");
+
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  if (!validateEmail(document.getElementById('Email1').value)) {
+    alert('Indirizzo email non valido!');
+    return;
+  }
+
+  // Invia l'email utilizzando EmailJS
+  emailjs.send("service_y6fyboq", "template_fd54yeu", {
+    Email1: document.getElementById('Email1').value,
+    messaggio: document.getElementById('messaggio').value,
+  }).then(function (response) {
+    console.log('SUCCESS!', response.status, response.text);
+    alert('Email inviata con successo!');
+  }, function (error) {
+    console.log('FAILED...', error);
+    alert('Errore nell\'invio dell\'email.');
+  });
+});
+
+// Funzione di validazione per l'indirizzo email
+function validateEmail(email) {
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
