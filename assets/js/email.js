6a2ec37bd87622ff622b3e1fbeb471cc4e551364
Gabri02 <132340142+Gabri02@users.noreply.github.com>
2023-05-14 00:35:11 +0200

    Update email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -25,28 +25,3 @@ function validateEmail(email) {
   var re = /\S+@\S+\.\S+/;
   return re.test(email);
 }
-
-document.getElementById('contact-form2').addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  if (!validateEmail(document.getElementById('Email2').value)) {
-    alert('Indirizzo email non valido!');
-    return;
-  }
-
-  emailjs.send("service_y6fyboq", "template_fd54yeu", {
-    Email2: document.getElementById('Email2').value,
-    messaggio2: document.getElementById('messaggio2').value,
-  }).then(function (response) {
-    console.log('SUCCESS!', response.status, response.text);
-    alert('Email inviata con successo!');
-  }, function (error) {
-    console.log('FAILED...', error);
-    alert('Errore nell\'invio dell\'email.');
-  });
-});
-
-function validateEmail(email) {
-  var re = /\S+@\S+\.\S+/;
-  return re.test(email);
-}
